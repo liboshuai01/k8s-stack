c02348d9ec0e89eb6163da528c25df24039edcc1
BoShuai Li <liboshuai01@gmail.com>
2025-05-18 02:26:35 +0800

    setup(helm): 添加 NFS 子目录外部供应器的 Helm 仓库并更新
    - 添加 NFS 子目录外部供应器的 Helm 仓库
    - 更新 Helm 仓库以获取最新的图表信息
    - 安装 NFS 子目录外部供应器

--- nfs-subdir-external-provisioner/install.sh
+++ nfs-subdir-external-provisioner/install.sh
@@ -2,6 +2,9 @@
 
 set -x
 
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+helm repo update
+
 helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
   --set nfs.server=master \
   --set nfs.path=/data/nfs/k8s \
