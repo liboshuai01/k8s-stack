b19d47f11c6180f3d4e3053985bb8ced24a90ea8
BoShuai Li <liboshuai01@gmail.com>
2025-05-25 14:23:53 +0800

    build(redis-cluster): 调整 Redis 集群资源限制
    - 将 master 和 replica 的 CPU 请求从 1 调整为 500m
    - 将 master 和 replica 的 CPU限制从 2 调整为 750m
    - 将 master 和 replica 的内存限制从 4096Mi调整为 2048Mi

--- redis-cluster/install.sh
+++ redis-cluster/install.sh
@@ -21,14 +21,14 @@ helm install "$HELM_RELEASE_NAME" bitnami/redis-cluster --version "$CHART_VERSIO
   --set-string global.storageClass="$STORAGE_CLASS" \
   --set-string global.redis.password="$REDIS_PASSWORD" \
   \
-  --set master.resources.requests.cpu=1 \
+  --set master.resources.requests.cpu=500m \
   --set master.resources.requests.memory=1024Mi \
-  --set master.resources.limits.cpu=2 \
-  --set master.resources.limits.memory=4096Mi \
+  --set master.resources.limits.cpu=750m \
+  --set master.resources.limits.memory=2048Mi \
   \
-  --set replica.resources.requests.cpu=1 \
+  --set replica.resources.requests.cpu=500m \
   --set replica.resources.requests.memory=1024Mi \
-  --set replica.resources.limits.cpu=2 \
-  --set replica.resources.limits.memory=4096Mi \
+  --set replica.resources.limits.cpu=750m \
+  --set replica.resources.limits.memory=2048Mi \
 
 echo "Redis Cluster 安装命令已执行。请使用 status.sh 检查状态。"
