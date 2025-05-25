fd58cc190aa06b44c569633ec20bc15c52689146
BoShuai Li <liboshuai01@gmail.com>
2025-05-25 13:07:46 +0800

    refactor(status.sh): 简化 status.sh 脚本以提高可维护性
    - 移除了 nfs-subdir-external-provisioner/status.sh 中特定于 nfs-storage 的筛选
    - 优化了 redis-cluster/status.sh 中的资源获取命令
    - 删除了不必要的输出和注释，使脚本更加简洁

--- nfs-subdir-external-provisioner/status.sh
+++ nfs-subdir-external-provisioner/status.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# 获取 StorageClass 列表并筛选出 nfs-storage
-kubectl get storageclass | grep nfs-storage
+# 获取 StorageClass 列表
+kubectl get storageclass
--- redis-cluster/status.sh
+++ redis-cluster/status.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-NAMESPACE="redis-cluster" # 与安装脚本中的 NAMESPACE 一致
+NAMESPACE="redis-cluster"
 
-echo "--- Helm Release 状态 ---"
-helm list -n "$NAMESPACE"
-
-echo ""
-echo "--- Pods 状态 (等待所有 Pods Running 且 Ready) ---"
-kubectl get pods -n "$NAMESPACE" -l app.kubernetes.io/name=redis-cluster -w
-
-# 如果想查看所有资源，可以使用:
-# kubectl get all -n "$NAMESPACE"
+kubectl get all -n ${NAMESPACE}
+kubectl get pvc -n ${NAMESPACE}
