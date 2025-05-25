655a35a7eda713d01d56447dbd5dcff24355682d
BoShuai Li <liboshuai01@gmail.com>
2025-05-25 14:26:20 +0800

    refactor(kafka-cluster & redis-cluster): 优化卸载脚本并提高可维护性
    - 为 kafka-cluster 和 redis-cluster 的卸载脚本引入变量，提高脚本的可维护性
    - 统一卸载过程的输出信息，使其更加清晰和一致
    - 在 redis-cluster 卸载脚本中移除具体的 PVC 删除命令，简化卸载过程
    - 优化脚本以适应不同的 reclaimPolicy 设置

--- kafka-cluster/uninstall.sh
+++ kafka-cluster/uninstall.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-helm uninstall kafka-cluster -n kafka-cluster
+HELM_RELEASE_NAME="kafka-cluster"
+NAMESPACE="kafka-cluster"
+
+helm uninstall ${HELM_RELEASE_NAME} -n ${NAMESPACE}
 
 echo "Kafka 集群卸载过程已启动。"
 echo "如果 reclaimPolicy 不是 Delete，Persistent Volume Claims (PVCs) 可能需要手动删除。"
-echo "请使用以下命令检查：kubectl get pvc -n kafka-cluster"
+echo "请使用以下命令检查：kubectl get pvc -n ${NAMESPACE}"
--- redis-cluster/uninstall.sh
+++ redis-cluster/uninstall.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 HELM_RELEASE_NAME="redis-cluster"
-NAMESPACE="redis-cluster" # 与安装脚本中的 NAMESPACE 一致
+NAMESPACE="redis-cluster"
 
 helm uninstall "$HELM_RELEASE_NAME" -n "$NAMESPACE"
 
-echo "Redis Cluster 已卸载。"
-echo "注意：持久卷声明 (PVCs) 可能仍然存在。如果需要删除数据，请手动删除它们："
-echo "kubectl get pvc -n $NAMESPACE -l app.kubernetes.io/instance=$HELM_RELEASE_NAME"
-echo "kubectl delete pvc -n $NAMESPACE -l app.kubernetes.io/instance=$HELM_RELEASE_NAME"
+echo "Redis 集群卸载过程已启动。"
+echo "如果 reclaimPolicy 不是 Delete，Persistent Volume Claims (PVCs) 可能需要手动删除。"
+echo "请使用以下命令检查：kubectl get pvc -n ${NAMESPACE}"
