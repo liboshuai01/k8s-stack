e9a54e044a9d6c60957a0b57accb5d5689c20aee
BoShuai Li <liboshuai01@gmail.com>
2025-05-26 14:30:49 +0800

    refactor(kafka-exporter): 修改状态脚本以简化命令
    - 移除了不必要的标签选择器，简化了 kubectl 命令
    - 将命名空间更改为与 RELEASE_NAME 一致，提高了灵活性

--- kafka-exporter/status.sh
+++ kafka-exporter/status.sh
@@ -3,5 +3,5 @@
 NAMESPACE="kafka"
 RELEASE_NAME="my-kafka-exporter"
 
-kubectl get all -n ${NAMESPACE} -l app.kubernetes.io/instance=${RELEASE_NAME}
-kubectl get pvc -n ${NAMESPACE} -l app.kubernetes.io/instance=${RELEASE_NAME}
+kubectl get all -n ${RELEASE_NAME}
+kubectl get pvc -n ${RELEASE_NAME}
