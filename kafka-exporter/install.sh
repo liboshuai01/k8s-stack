6b9dacc2b93115943560abf8a2daca8864cb4303
BoShuai Li <liboshuai01@gmail.com>
2025-05-26 20:53:20 +0800

    refactor(kafka-exporter):重构安装脚本并添加环境变量配置
    - 新增 .env 文件，用于存储配置变量
    - 更新 install.sh 脚本，使用环境变量替代硬编码值
    - 修改 status.sh 和 uninstall.sh脚本，通过环境变量获取命名空间和 release 名称
    - 优化脚本结构，提高可维护性和可扩展性

--- kafka-exporter/install.sh
+++ kafka-exporter/install.sh
@@ -1,33 +1,27 @@
 #!/usr/bin/env bash
 
+# --- 加载变量 ---
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+else
+    echo "错误: .env 文件不存在!"
+    exit 1
+fi
+
 # 添加库并更新
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
-# --- 配置变量 ---
-RELEASE_NAME="my-kafka-exporter"
-NAMESPACE="kafka"
-CHART_VERSION="2.12.1"
-
 # --- 安装命令 ---
-helm upgrade --install ${RELEASE_NAME} prometheus-community/prometheus-kafka-exporter --version ${CHART_VERSION} \
-  --namespace ${NAMESPACE} \
-  --create-namespace \
+helm upgrade --install ${RELEASE_NAME} prometheus-community/prometheus-kafka-exporter \
+  --version ${CHART_VERSION} --namespace ${NAMESPACE} --create-namespace \
   \
-  --set kafkaServer[0]="my-kafka-cluster-controller-0.my-kafka-cluster-controller-headless.kafka.svc.cluster.local:9092" \
-  --set kafkaServer[1]="my-kafka-cluster-controller-1.my-kafka-cluster-controller-headless.kafka.svc.cluster.local:9092" \
-  --set kafkaServer[2]="my-kafka-cluster-controller-2.my-kafka-cluster-controller-headless.kafka.svc.cluster.local:9092" \
+  --set kafkaServer[0]="${KAFKA_SERVER}" \
   --set prometheus.serviceMonitor.enabled=true \
-  --set prometheus.serviceMonitor.namespace=monitoring \
-  --set prometheus.serviceMonitor.additionalLabels.release=kube-prom-stack \
+  --set prometheus.serviceMonitor.namespace="${MONITOR_NAMESPACE}" \
+  --set prometheus.serviceMonitor.additionalLabels.release="${MONITOR_RELEASE_NAME}" \
   \
   --set resources.requests.cpu=100m \
   --set resources.requests.memory=128Mi \
   --set resources.limits.cpu=1000m \
-  --set resources.limits.memory=2048Mi \
-
-echo ""
-echo "${RELEASE_NAME} 安装/升级过程已启动到命名空间 '${NAMESPACE}'。"
-echo "---------------------------------------------------------------------"
-echo "监控 Pod 状态: kubectl get pods -n ${NAMESPACE} -w"
-echo "---------------------------------------------------------------------"
+  --set resources.limits.memory=2048Mi
