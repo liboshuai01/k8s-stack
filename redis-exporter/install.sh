5d320ba11ccaf35e1f7851ba4d8455d9ad23c431
BoShuai Li <liboshuai01@gmail.com>
2025-05-26 15:00:26 +0800

    refactor(redis-exporter):简化 install.sh 脚本并优化默认配置
    - 移除了不必要的注释和变量定义
    - 调整了 helm upgrade --install 命令的格式，使其更加清晰
    -硬编码了 Redis 地址和 Secret 配置，减少了用户输入的需求
    - 更新了监控 Pod状态的命令，使其适用于所有 Redis Exporter 相关的 Pod

--- redis-exporter/install.sh
+++ redis-exporter/install.sh
@@ -5,25 +5,15 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 
 # --- 配置变量 ---
-RELEASE_NAME="my-redis-exporter"  # Redis Exporter 的发布名称
-NAMESPACE="redis"            # Redis Exporter 将被安装到的命名空间
-CHART_VERSION="6.10.3"            # 根据需要调整到最新或兼容版本，可通过 'helm search repo prometheus-community/redis-exporter' 查看
-
-# Redis Cluster 的服务地址，根据 'kubectl get service -n redis' 的输出
-# 格式为 redis://<service-name>.<namespace>:<port>
-REDIS_CLUSTER_ADDRESS="redis://my-redis-cluster-0.my-redis-cluster-headless.redis.svc.cluster.local:6379"
-
-# Secret 名称和其中密码的 Key
-REDIS_SECRET_NAME="my-redis-cluster"
-# 根据你的 Secret 输出，密码的 Key 是 'redis-password'
-REDIS_SECRET_KEY="redis-password"
+NAMESPACE="redis"
+RELEASE_NAME="my-redis-exporter"
+CHAR_VERSION="6.10.3"
 
 # --- 安装/升级命令 ---
-helm upgrade --install ${RELEASE_NAME} prometheus-community/prometheus-redis-exporter --version ${CHART_VERSION} \
-  --namespace ${NAMESPACE} \
-  --create-namespace \
+helm upgrade --install ${RELEASE_NAME} prometheus-community/prometheus-redis-exporter \
+  --version ${CHAR_VERSION} --namespace ${NAMESPACE} --create-namespace \
   \
-  --set redisAddress="${REDIS_CLUSTER_ADDRESS}" \
+  --set redisAddress="redis://my-redis-cluster:6379" \
   \
   --set serviceMonitor.enabled=true \
   --set serviceMonitor.namespace=monitoring \
@@ -35,11 +25,11 @@ helm upgrade --install ${RELEASE_NAME} prometheus-community/prometheus-redis-exp
   --set resources.limits.memory=2048Mi \
   \
   --set auth.enabled=true \
-  --set auth.secret.name="${REDIS_SECRET_NAME}" \
-  --set auth.secret.key="${REDIS_SECRET_KEY}" \
+  --set auth.secret.name="my-redis-cluster" \
+  --set auth.secret.key="redis-password" \
 
 echo ""
 echo "${RELEASE_NAME} 安装/升级过程已启动到命名空间 '${NAMESPACE}'。"
 echo "---------------------------------------------------------------------"
-echo "监控 Pod 状态: kubectl get pods -n ${NAMESPACE} -l app.kubernetes.io/name=prometheus-redis-exporter -w"
+echo "监控 Pod 状态: kubectl get pods -n ${NAMESPACE} -w"
 echo "---------------------------------------------------------------------"
