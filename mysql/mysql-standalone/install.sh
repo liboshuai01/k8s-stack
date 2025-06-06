1b0311485ce51a68b5697f263f7daf400b073b5a
Boshuai Li <liboshuai01@gmail.com>
2025-06-07 03:18:36 +0800

    feat(mysql): 添加 MySQL 单节点部署脚本和文档
    - 新增 .env 文件用于配置 MySQL部署参数
    - 添加 install.sh 脚本实现 MySQL单节点部署
    - 编写 README.md 文档说明部署步骤和验证方法
    - 实现 status.sh 脚本用于检查 MySQL部署状态
    - 添加 uninstall.sh 脚本用于卸载 MySQL 部署

---
+++ mysql/mysql-standalone/install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# --- 加载变量 ---
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | sed 's/\r$//' | xargs)
+else
+    echo "错误: .env 文件不存在!"
+    exit 1
+fi
+
+# --- 添加仓库并更新 ---
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+
+# --- 安装 / 升级 ---
+helm upgrade --install ${RELEASE_NAME} bitnami/mysql --version ${CHART_VERSION} \
+  --namespace ${NAMESPACE} \
+  --create-namespace \
+  \
+  --set-string global.storageClass="${STORAGE_CLASS_NAME}" \
+  \
+  --set-string auth.rootPassword=${MYSQL_ROOT_PASSWORD} \
+  --set-string auth.database=${MYSQL_DATABASE} \
+  --set-string auth.username=${MYSQL_USERNAME} \
+  --set-string auth.password=${MYSQL_PASSWORD} \
+  \
+  --set primary.persistence.size=8Gi \
+  \
+  --set primary.resources.requests.cpu=250m \
+  --set primary.resources.requests.memory=512Mi \
+  --set primary.resources.limits.cpu=2000m \
+  --set primary.resources.limits.memory=4096Mi
