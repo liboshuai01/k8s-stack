91c4c2420b4dc71cc747ef21fea964694d744ad9
Boshuai Li <liboshuai01@gmail.com>
2025-06-12 01:56:49 +0800

    docs(nginx): 添加 nginx-standalone 和 nginx-cluster 的部署文档
    - 新增 nginx-standalone 和 nginx-cluster 的 .env、install.sh、README.md、status.sh 和 uninstall.sh 文件
    - 提供了详细的部署前提准备、安装应用、验证应用、更新应用和卸载应用的步骤说明
    - 部署文档中包含了 Prometheus 监控组件的配置和验证方法

---
+++ nginx/nginx-cluster/install.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -e
+
+# --- 加载变量 ---
+if [ -f .env ]; then
+    source .env
+else
+    echo "错误: .env 文件不存在!"
+    exit 1
+fi
+
+# --- 添加仓库并更新 ---
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+
+# --- 安装 / 升级 ---
+helm upgrade --install ${RELEASE_NAME} bitnami/nginx \
+  --version ${CHART_VERSION} --namespace ${NAMESPACE} --create-namespace \
+  \
+  --set replicaCount=${REPLICA_COUNT} \
+  --set resources.requests.cpu=100m \
+  --set resources.requests.memory=128Mi \
+  --set resources.limits.cpu=512m \
+  --set resources.limits.memory=2048Mi \
+  \
+  --set service.type=${SERVICE_TYPE} \
+  \
+  --set ingress.enabled=${INGRESS_ENABLED} \
+  --set ingress.ingressClassName=${INGRESS_CLASS_NAME} \
+  --set ingress.hostname=${NGINX_HOST} \
+  --set ingress.path="/" \
+  \
+  --set metrics.enabled=true \
+  --set metrics.serviceMonitor.enabled=true \
+  --set metrics.serviceMonitor.namespace="${PROMETHEUS_NAMESPACE}" \
+  --set metrics.serviceMonitor.labels.release="${PROMETHEUS_RELEASE_LABEL}" \
+  --set metrics.resources.requests.cpu=100m \
+  --set metrics.resources.requests.memory=128Mi \
+  --set metrics.resources.limits.cpu=256m \
+  --set metrics.resources.limits.memory=1024Mi
---
+++ nginx/nginx-cluster/status.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+# --- 加载变量 ---
+if [ -f .env ]; then
+    source .env
+else
+    echo "错误: .env 文件不存在!"
+    exit 1
+fi
+
+kubectl get all -n ${NAMESPACE}
+kubectl get pvc -n ${NAMESPACE}
