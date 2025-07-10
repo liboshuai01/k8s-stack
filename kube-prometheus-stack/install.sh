6bed45462462b09075f32b0bc3680c92b82e30ae
Boshuai Li <liboshuai01@gmail.com>
2025-07-10 21:10:21 +0800

    refactor(kube-prometheus-stack): 重构安装脚本并添加 values.yml 示例
    - 重新组织安装脚本，优化变量加载和错误处理
    - 添加 values.yml 示例文件，简化配置过程
    - 更新安装命令，使用 values.yml 替代命令行参数
    - 增加成功部署提示信息

--- kube-prometheus-stack/install.sh
+++ kube-prometheus-stack/install.sh
@@ -3,11 +3,17 @@
 set -e
 set -o pipefail
 
-# --- 加载变量 ---
+# --- 加载环境变量 ---
 if [ -f .env ]; then
     source .env
 else
     echo "错误: .env 文件不存在!"
+    exit 1
+fi
+
+# --- 检查 values.yml 文件是否存在 ---
+if [ ! -f values.yml ]; then
+    echo "错误: values.yml 文件不存在!"
     exit 1
 fi
 
@@ -18,34 +24,7 @@ helm repo update
 # --- 安装 / 升级 ---
 helm upgrade --install ${RELEASE_NAME} prometheus-community/kube-prometheus-stack \
   --version ${CHART_VERSION} --namespace ${NAMESPACE} --create-namespace \
-  \
-  --set alertmanager.enabled=true \
-  --set alertmanager.ingress.enabled=true \
-  --set alertmanager.ingress.ingressClassName=${INGRESS_CLASS_NAME} \
-  --set alertmanager.ingress.hosts[0]=${ALERTMANAGER_HOST} \
-  --set alertmanager.ingress.paths[0]="/" \
-  --set alertmanager.alertmanagerSpec.storage.volumeClaimTemplate.spec.storageClassName=${STORAGE_CLASS_NAME} \
-  --set alertmanager.alertmanagerSpec.storage.volumeClaimTemplate.spec.accessModes[0]="ReadWriteOnce" \
-  --set alertmanager.alertmanagerSpec.storage.volumeClaimTemplate.spec.resources.requests.storage=8Gi \
-  \
-  --set prometheus.enabled=true \
-  --set prometheus.ingress.enabled=true \
-  --set prometheus.ingress.ingressClassName=${INGRESS_CLASS_NAME} \
-  --set prometheus.ingress.hosts[0]=${PROMETHEUS_HOST} \
-  --set prometheus.ingress.paths[0]="/" \
-  --set prometheus.prometheusSpec.storageSpec.volumeClaimTemplate.spec.storageClassName=${STORAGE_CLASS_NAME} \
-  --set prometheus.prometheusSpec.storageSpec.volumeClaimTemplate.spec.accessModes[0]="ReadWriteOnce" \
-  --set prometheus.prometheusSpec.storageSpec.volumeClaimTemplate.spec.resources.requests.storage=32Gi \
-  \
-  --set grafana.enabled=true \
-  --set grafana.adminPassword=${GRAFANA_ADMIN_PASSWORD} \
-  --set grafana.ingress.enabled=true \
-  --set grafana.ingress.ingressClassName=${INGRESS_CLASS_NAME} \
-  --set grafana.ingress.hosts[0]=${GRAFANA_HOST} \
-  --set grafana.ingress.path="/" \
-  --set grafana.persistence.enabled=true \
-  --set grafana.persistence.storageClassName=${STORAGE_CLASS_NAME} \
-  --set grafana.persistence.accessModes[0]="ReadWriteOnce" \
-  --set grafana.persistence.size=8Gi \
-  \
-  --set prometheusOperator.enabled=true
+  -f values.yml
+
+echo "Helm Chart '${RELEASE_NAME}' 已成功部署到命名空间 '${NAMESPACE}' 中。"
+
