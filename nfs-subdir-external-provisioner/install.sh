f3027a2cd9dbf6e8c8c67e5fa85bd353c173e939
Boshuai Li <liboshuai01@gmail.com>
2025-07-10 19:53:47 +0800

    feat(install): 更新安装脚本和文档以使用 values.yaml
    - 在 install.sh 中添加 values.yaml 文件存在性检查
    - 修改 install.sh 以使用 -f values.yaml 加载配置参数
    - 更新 README.md 中的安装步骤说明
    - 新增 values.yaml.example 作为配置示例

--- nfs-subdir-external-provisioner/install.sh
+++ nfs-subdir-external-provisioner/install.sh
@@ -11,11 +11,20 @@ else
     exit 1
 fi
 
+# --- 检查 values.yaml 文件是否存在 ---
+if [ ! -f values.yaml ]; then
+    echo "错误: values.yaml 文件不存在!"
+    exit 1
+fi
+
 # --- 添加仓库并更新 ---
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
 helm repo update
 
 # --- 安装 / 升级 ---
-helm upgrade --install "${RELEASE_NAME}" nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
-  --version "${CHART_VERSION}" --namespace "${NAMESPACE}" --create-namespace \
-  "${HELM_SET_ARGS[@]}"
+# 使用 -f values.yaml 来加载所有配置参数
+helm upgrade --install ${RELEASE_NAME} nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+  --version ${CHART_VERSION} --namespace ${NAMESPACE} --create-namespace \
+  -f values.yaml
+
+echo "Helm Chart '${RELEASE_NAME}' 已成功部署到命名空间 '${NAMESPACE}' 中。"
