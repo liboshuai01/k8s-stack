9dcd84d6c1b9c907445907e7c90a648743816b76
Boshuai Li <liboshuai01@gmail.com>
2025-07-11 14:56:21 +0800

    refactor(install): 更新安装脚本中的配置文件检查逻辑
    - 将 values.yml 文件检查更改为 config.yml 文件检查
    - 更新错误消息以匹配新的配置文件名称

--- metallb/install.sh
+++ metallb/install.sh
@@ -11,9 +11,9 @@ else
     exit 1
 fi
 
-# --- 检查 values.yml 文件是否存在 ---
-if [ ! -f values.yml ]; then
-    echo "错误: values.yml 文件不存在!"
+# --- 检查 config.yml 文件是否存在 ---
+if [ ! -f config.yml ]; then
+    echo "错误: config.yml 文件不存在!"
     exit 1
 fi
 
